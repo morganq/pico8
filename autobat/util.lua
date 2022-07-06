cef82c29fbc1d5c1819365aaaa0ae615a49d47ec
morganq <morganquirk@gmail.com>
2022-07-06 11:12:26 -0700

    fail

--- autobat/util.lua
+++ autobat/util.lua
@@ -12,12 +12,3 @@ function roundrect(x1,y1,x2,y2)
     rectfill(x1,y1+1,x2,y2-1,5)
     rectfill(x1+1,y1,x2-1,y2,5)
 end
-
-function interp(s, ...)
-    local parts = split(s, "%")
-    local r = parts[1]
-    for i,v in ipairs(arg) do
-        r ..= v .. parts[i + 1]
-    end
-    return r
-end
